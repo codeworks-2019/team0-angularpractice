7bb02ca5b3eef9596802d6f63d8cf77eb2c3d713
jtodaroii <30529065+jtodaroii@users.noreply.github.com>
2019-07-16 11:19:51 -0400

    added a check for null for inputElement

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var inputElement = document.getElementById("dataToBind");
 var showElement = document.getElementById("showBoundData");
 
 // Add an event listener to listen for keyup event
-inputElement.addEventListener("keyup", function() {
-  showElement.innerHTML = inputElement.value.toString();
+if(inputElement){
+  inputElement.addEventListener("keyup", function() {
+    showElement.innerHTML = inputElement.value.toString();
 });
